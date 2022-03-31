35907e65ce6ee02c72cd3fe7095d2ff67adc0bde
Aline Souza <ds.alinesouza@gmail.com>
2022-03-30 21:50:31 -0300

    estiliza login

--- src/components/App.js
+++ src/components/App.js
@@ -1,28 +1,31 @@
-import React from "react"
-import {Container} from "react-bootstrap"
-import SignUp from "./SignUp"
-import {AuthProvider} from "../contexts/AuthContext"
-import {BrowserRouter, Switch, Route} from "react-router-dom"
-import Home from "./Home"
-import Login from "./Login"
-import PrivateRoute from "./PrivateRoute"
-
+import React from "react";
+import { Container } from "react-bootstrap";
+import SignUp from "./SignUp";
+import { AuthProvider } from "../contexts/AuthContext";
+import { BrowserRouter, Switch, Route } from "react-router-dom";
+import Home from "./Home";
+import Login from "./Login";
+import PrivateRoute from "./PrivateRoute";
+import "../style.css";
 function App() {
   return (
-    <Container className="d-flex align-items-center justify-content-center" style={{minHeight:"100vh"}}>
-      <div className="w-100" style={{maxWidth: "400px"}}>
+    <div
+      className="d-flex align-items-center justify-content-center"
+      style={{ minHeight: "100vh" }}
+    >
+      <div className="w-100" style={{ maxWidth: "400px" }}>
         <BrowserRouter>
           <AuthProvider>
             <Switch>
               <PrivateRoute exact path="/" component={Home} />
               <Route path="/login" component={Login} />
-              <Route path="/signUp" component={SignUp} />               
+              <Route path="/signUp" component={SignUp} />
             </Switch>
           </AuthProvider>
         </BrowserRouter>
       </div>
-    </Container>
-  )
+    </div>
+  );
 }
 
-export default App
+export default App;
