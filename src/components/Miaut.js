807bdf1edbb587846b2473b5ab4054e2a89635af
Renan Marcilio <68852560+marcdevrenan@users.noreply.github.com>
2022-03-31 20:47:37 -0300

    Add files via upload

---
+++ src/components/Miaut.js
@@ -0,0 +1,17 @@
+import React from "react"
+import {Link} from "react-router-dom"
+import logo from "../img/logoMiaut.png"
+
+export default function Miaut() {
+    return (
+        <div>
+            <div>
+                <div>
+                    <img src={logo} id="logo" alt="Logo" />
+                </div>
+                <Link to="/login" id="button-orange" className="btn btn-default w-100 mt-4" type="submit">Entrar</Link>
+                <Link id="align" className="link-none-decoration" to="/signUp">Primeiro Acesso</Link>
+            </div>
+        </div>
+    )
+}
